4c63d5ca3db94df46a9d413d888c53a78447c726
Estase <estase3@gmail.com>
2021-08-18 21:29:58 +0200

    Added funcionality to close popups

--- Tutorial/main.py
+++ Tutorial/main.py
@@ -5,35 +5,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
+import os
+
+def takeScreenshot(driver):
+    """Takes ss of site and saves to specified folder named as number."""
+    ss_num = 0
+    ssPATH = "E:\Programowanie\Python\selenium\Tutorial\ss\\" + str(ss_num) + ".png"
+    while os.path.isfile(ssPATH):
+        ss_num += 1
+        ssPATH = "E:\Programowanie\Python\selenium\Tutorial\ss\\" + str(ss_num) + ".png"
+    driver.save_screenshot(ssPATH)
+
 
 def getMaterials(driver):
-    wood = int(driver.find_elements_by_id("wood")[0].text)
-    stone = int(driver.find_elements_by_id("stone")[0].text)
-    iron = int(driver.find_elements_by_id("iron")[0].text)
-    storage = int(driver.find_elements_by_id("storage")[0].text)
-    return wood, stone, iron, storage
-    
+    """Gets informations about quanity of materials"""
+    materials = {
+    "wood" : int(driver.find_elements_by_id("wood")[0].text),
+    "stone" : int(driver.find_elements_by_id("stone")[0].text),
+    "iron" : int(driver.find_elements_by_id("iron")[0].text),
+    "storage" : int(driver.find_elements_by_id("storage")[0].text)
+    }
+    return materials
+
+def closePopup(driver):
+    """Closes mission and daily bonus popups"""
+    driver.find_element_by_xpath("//a[@class='popup_box_close tooltip-delayed']").click()
+
 
 PATH = "E:\Programowanie\Python\selenium\chromedriver.exe"
-
 opt = Options()
 opt.add_argument("user-data-dir=E:\\Programowanie\\Python\\selenium\\Tutorial\\tmp_cache")
 driver = webdriver.Chrome(executable_path = PATH, options=opt)
+driver.get("https://www.plemiona.pl/page/play/pl167")
 
-driver.get("https://www.plemiona.pl")
-try:
-    world = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//span[@class='world_button_active']"))
-    )
-    world.click()
+try: 
+    driver.find_elements_by_id("manager_icon_farm")[0].click()
 except:
-    pass
+    takeScreenshot(driver)
+    closePopup(driver)
+    takeScreenshot(driver)
+    try:
+        driver.find_elements_by_id("manager_icon_farm")[0].click()
+    except:
+        takeScreenshot(driver)
+        driver.quit()
 
-wood, stone, iron, storage = getMaterials(driver)
 
-driver.find_elements_by_id("manager_icon_farm")[0].click()
+try:
+    A_btns=driver.find_elements_by_xpath("//a[contains(@onclick,'8510')]")
+except:
+    driver.quit()
 
-A_btns=driver.find_elements_by_xpath("//a[contains(@onclick,'8510')]")
+
 for x in range(13):
     A_btns[x].click()
     time.sleep(0.5)
