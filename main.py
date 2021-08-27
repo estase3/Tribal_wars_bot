764ce382a0bbfe4b0007b03e174d513884d3bba5
Estase <estase3@gmail.com>
2021-08-27 22:55:45 +0200

    Added possibility to run scav once, cleared some code

---
+++ main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from scavenging import *
+import time
+import os
+
+PATH = "E:\Programowanie\Python\selenium\chromedriver.exe"
+opt = Options()
+opt.add_argument("user-data-dir=E:\\Programowanie\\Python\\selenium\\Tutorial\\tmp_cache")      #Gets cache needed to login automatically
+driver = webdriver.Chrome(executable_path = PATH, options=opt)
+#driver.get("https://www.plemiona.pl/page/play/pl167")
+driver.get("https://www.plemiona.pl/page/play/plp7")
+
+
+def takeScreenshot(driver):
+    """Takes ss of site and saves to specified folder named as number."""
+    ss_num = 0
+    ssPATH = "E:\Programowanie\Python\selenium\Tutorial\ss\\" + str(ss_num) + ".png"
+    while os.path.isfile(ssPATH):
+        ss_num += 1
+        ssPATH = "E:\Programowanie\Python\selenium\Tutorial\ss\\" + str(ss_num) + ".png"
+    driver.save_screenshot(ssPATH)
+
+
+def closePopup(driver):
+    """Closes mission and daily bonus popups"""
+    driver.find_element_by_xpath("//a[@class='popup_box_close tooltip-delayed']").click()
+
+scavenge(driver)
---
+++ scavenging.py
@@ -0,0 +1,143 @@
+import math
+import time
+from getters import *
+
+
+def fourthLvlScav(driver, choosenTroops, sums):
+    print("4th wtf")
+    sendBtns = driver.find_elements_by_xpath("//a[@class ='btn btn-default free_send_button']")
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/13)
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[3].click()
+    time.sleep(1)
+
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/13 * 1.5)
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[2].click()
+    time.sleep(1)
+
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/13 * 1.5 * 2)
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[1].click()
+    time.sleep(1)
+
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/13 * 1.5 * 2 * 2.5)
+        if sums[troop] + val > choosenTroops[troop]:
+            val = choosenTroops[troop] - sums[troop]
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[0].click()
+
+
+def thirdLvlScav(driver, choosenTroops, sums):
+    print("3rd")
+    sendBtns = driver.find_elements_by_xpath("//a[@class ='btn btn-default free_send_button']")
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/8)
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[2].click()
+    time.sleep(1)
+
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/8 * 2)
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[1].click()
+    time.sleep(1)
+    
+    for troop in choosenTroops:
+        val = choosenTroops[troop] - sums[troop]
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[0].click()
+
+
+def secondLvlScav(driver, choosenTroops, sums):
+    print("2nd")
+    sendBtns = driver.find_elements_by_xpath("//a[@class ='btn btn-default free_send_button']")
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/3.5)
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[1].click()
+    time.sleep(1)
+    
+    for troop in choosenTroops:
+        val = math.ceil(choosenTroops[troop]/3.5 * 2.5)
+        if sums[troop] + val > choosenTroops[troop]:
+            val = choosenTroops[troop] - sums[troop]
+        driver.find_element_by_xpath("//input[@name='"+troop+"']").send_keys(val)
+        sums[troop] += val
+        time.sleep(0.2)
+    sendBtns[0].click()
+
+def getAvaibScavLevels(driver):
+    time.sleep(1)
+    levels = driver.find_elements_by_xpath("//a[@class ='btn btn-default unlock-button']")
+    return 4 - len(levels)
+
+
+def runScav(driver,choosenTroops, sums, level):
+    print(level)
+    if level == 4:
+        fourthLvlScav(driver, choosenTroops, sums)
+    elif level == 3:
+        thirdLvlScav(driver, choosenTroops, sums)
+    elif level == 2:
+        secondLvlScav(driver, choosenTroops, sums)
+    
+
+def scavenge(driver):
+
+    sums = {
+        "spear"     : 0,
+        "sword"     : 0,
+        "axe"       : 0,
+        "archer"    : 0,
+        "light"     : 0,
+        "marcher"   : 0,
+        "heavy"     : 0
+        }
+
+
+    avaiableTroops = getTroops(driver)
+    choosenTroops = {
+        "sword" : 0,
+        "spear" : 0,
+        "axe"   : 0
+    }
+    #troops that user wants to go to scavenging
+    #TO DO: implement getting choosenTroops from cfg file
+    #setting choosenTroop as 0 will set all avaiable troops.
+
+
+    for troop in list(choosenTroops):
+        if troop not in avaiableTroops:
+            del choosenTroops[troop]
+            continue 
+        if choosenTroops[troop] == 0:
+            choosenTroops[troop] = avaiableTroops[troop]
+        elif choosenTroops[troop] > avaiableTroops[troop]:
+            choosenTroops[troop] = avaiableTroops[troop]
+
+    driver.find_element_by_xpath("//area[contains(@href,'screen=place')]").click()       
+    driver.find_elements_by_xpath("//a[contains(@href,'&screen=place&mode=scavenge')]")[0].click()
+    runScav(driver, choosenTroops, sums, getAvaibScavLevels(driver))
+
+
